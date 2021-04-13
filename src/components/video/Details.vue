4595b2c545ba4866a6fa9cfa0f5187dd722c0fc4
francoism90 <f.menning@pm.me>
2021-04-13 13:08:02 +0200

    wip

--- src/components/video/Details.vue
+++ src/components/video/Details.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="container">
-    <h1 class="text-h3 text-white">
+    <h1 class="text-h2 text-white ellipsis-2-lines">
       {{ video.name }}
     </h1>
+
+    <div class="q-gutter-sm">
+      <q-chip
+        class="no-padding"
+        color="transparent"
+        icon="visibility"
+        dense
+        dark
+      >
+        <span class="q-ml-xs text-body1">{{ video.views || 0 }} views</span>
+      </q-chip>
+    </div>
+
     <p>{{ video.overview }}</p>
   </div>
 </template>
