7e468971182e3dd0d812ed570705917cab4a055b
francoism90 <f.menning@pm.me>
2021-04-17 10:09:06 +0200

    wip

--- src/components/player/PlaybackControl.vue
+++ src/components/player/PlaybackControl.vue
@@ -1,31 +1,45 @@
 <template>
-  <div ref="domRef">
-    <control
-      class="absolute-center"
-      keys="k"
-      label="Play/pause"
-      :pressed="paused"
-      @click="onClick"
-    >
+  <div
+    ref="domRef"
+    class="absolute-center"
+  >
+    <div class="q-gutter-md row no-wrap justify-center items-center content-center">
+      <q-icon
+        name="replay_10"
+        class="cursor-pointer"
+        size="48px"
+        color="white"
+        @click="onSeekBackward"
+      />
+
       <q-icon
         :name="icon"
         size="96px"
         color="white"
+        @click="onToggleClick"
       />
-    </control>
+
+      <q-icon
+        name="forward_10"
+        class="cursor-pointer"
+        size="48px"
+        color="white"
+        @click="onSeekForward"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Control, usePlayer, usePlayerContext } from '@vime/vue-next'
+import { usePlayer, usePlayerContext } from '@vime/vue-next'
 import { Video } from 'src/interfaces/video'
-import { computed, defineComponent, PropType, ref } from 'vue'
+import { computed, defineComponent, PropType, ref, watch } from 'vue'
 
 export default defineComponent({
   name: 'PlaybackControl',
 
   components: {
-    Control
+
   },
 
   props: {
@@ -42,16 +56,35 @@ export default defineComponent({
     const paused = usePlayerContext(domRef, 'paused', true)
     const icon = computed(() => paused.value ? 'play_arrow' : 'pause')
 
-    const onClick = () => {
-      paused.value = !paused.value
-    }
+    const currentTime = usePlayerContext(domRef, 'currentTime', 0)
+    const duration = usePlayerContext(domRef, 'duration', -1)
 
     return {
       domRef,
       player,
       paused,
-      onClick,
+      currentTime,
+      duration,
       icon
+    }
+  },
+
+  methods: {
+    onToggleClick () {
+      this.paused = !this.paused
+    },
+
+    onSeekBackward () {
+      console.log(this.currentTime)
+      if (this.currentTime < 10) return
+      this.currentTime -= 10
+    },
+
+    onSeekForward () {
+      console.log(this.currentTime)
+      console.log(this.duration)
+      if (this.currentTime > (this.duration - 10)) return
+      this.currentTime += 10
     }
   }
 })
