3bac62a0cc8393e72c20d683d04d42d7aabc22a9
francoism90 <f.menning@pm.me>
2021-04-19 13:07:11 +0200

    Use user-id to control app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <router-view />
+  <router-view :key="user.id || 1" />
 </template>
 
 <script lang="ts">
+import useSession from 'src/composables/useSession'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+
+  setup () {
+    const { user } = useSession()
+
+    return {
+      user
+    }
+  }
 })
 </script>
