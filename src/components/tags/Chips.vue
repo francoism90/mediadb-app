b9ae1f5950f4156d63dd1ed472a9a9200f4e19cc
francoism90 <f.menning@pm.me>
2021-04-23 20:23:04 +0200

    Add tag search

--- src/components/tags/Chips.vue
+++ src/components/tags/Chips.vue
@@ -11,11 +11,14 @@
       class="text-caption"
       size="0.875em"
       square
+      @click="onClick(tag)"
     />
   </div>
 </template>
 
 <script lang="ts">
+import useRepository from 'src/composables/useRepository'
+import useRouter from 'src/composables/useRouter'
 import { Tag } from 'src/interfaces/tag'
 import { defineComponent, PropType } from 'vue'
 
@@ -27,6 +30,24 @@ export default defineComponent({
       type: Array as PropType<Tag[]>,
       required: false,
       default: () => (<Tag[]>[])
+    }
+  },
+
+  setup () {
+    const { router } = useRouter()
+    const { setParams: setModuleParams } = useRepository({ module: 'videos' })
+
+    const onClick = async (tag: Tag) => {
+      await setModuleParams({
+        params: { 'filter[query]': tag.name, 'page[number]': 1 },
+        reset: true
+      })
+
+      await router.push({ name: 'home' })
+    }
+
+    return {
+      onClick
     }
   }
 })
