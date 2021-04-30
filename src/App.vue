6c3124e310691dfe6e3b795a65182dcb63c7e7d9
francoism90 <f.menning@pm.me>
2021-04-30 12:37:38 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
 
 <script lang="ts">
 import { useQuasar } from 'quasar'
-import { defineComponent, watch } from 'vue'
+import { defineComponent, onMounted, watch } from 'vue'
+import { Plugins } from '@capacitor/core';
+
+const { StatusBar } = Plugins;
 
 export default defineComponent({
   name: 'App',
@@ -13,15 +16,39 @@ export default defineComponent({
   setup () {
     const $q = useQuasar()
 
+    const screenOrientationLandscape = async (): Promise<void> => {
+      await window.screen.orientation.lock('landscape')
+    }
+
+    const screenOrientationUnlock = (): void => {
+      window.screen.orientation.unlock()
+    }
+
+    const hideStatusBar = async (): Promise<void> => {
+      await StatusBar.hide()
+    }
+
+    const showStatusBar = async (): Promise<void> => {
+      await StatusBar.show()
+    }
+
+    onMounted(async () => {
+      // Display content under transparent status bar (Android only)
+      await StatusBar.setOverlaysWebView({ overlay: true })
+    }),
+
     watch(() => $q.fullscreen.isActive, async (value): Promise<void> => {
       if (!$q.platform.is.capacitor && !$q.platform.is.cordova) {
         return
       }
 
       if (value === true) {
-        await window.screen.orientation.lock('landscape')
+        await hideStatusBar()
+        await screenOrientationLandscape()
+
       } else {
-        window.screen.orientation.unlock()
+        await showStatusBar()
+        screenOrientationUnlock()
       }
     })
   }
