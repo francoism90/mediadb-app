f2b5202c5f88358a2fac480ac18b0414f30648d3
francoism90 <francoism90@users.noreply.github.com>
2021-06-26 09:36:26 +0200

    wip

--- src/components/videos/Filters.vue
+++ src/components/videos/Filters.vue
@@ -3,47 +3,72 @@
     ref="dialogRef"
     @hide="onDialogHide"
   >
-    <q-list
-      dense
-      class="video-filters"
-    >
-      <q-item-label
-        header
-        class="video-filters-header"
+    <div class="video-filter-container">
+      <q-list
+        dense
+        class="video-filter-list"
       >
-        Content filtering
-      </q-item-label>
+        <q-item-label
+          header
+          class="video-filter-search"
+        >
+          <q-input
+            v-model="store.query.filter.query"
+            autofocus
+            borderless
+            color="grey-10"
+            clearable
+            debounce="500"
+            dense
+            hide-bottom-space
+            input-class="text-caption"
+            placeholder="Search videos.."
+            type="text"
+          >
+            <template #prepend>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </q-item-label>
 
-      <q-item
-        v-ripple
-        tag="label"
-      >
-        <q-item-section side>
-          <q-checkbox
-            v-model="store.query.filter.favorites"
-            size="sm"
-            :true-value="1"
-            :false-value="null"
-          />
-        </q-item-section>
-        <q-item-section>Bookmarks</q-item-section>
-      </q-item>
+        <q-item-label
+          header
+          class="video-filter-header"
+        >
+          Filter List
+        </q-item-label>
 
-      <q-item
-        v-ripple
-        tag="label"
-      >
-        <q-item-section side>
-          <q-checkbox
-            v-model="store.query.filter.favorites"
-            size="sm"
-            :true-value="1"
-            :false-value="null"
-          />
-        </q-item-section>
-        <q-item-section>Watchlist</q-item-section>
-      </q-item>
-    </q-list>
+        <q-item
+          v-ripple
+          tag="label"
+        >
+          <q-item-section side>
+            <q-checkbox
+              v-model="store.query.filter.favorites"
+              size="sm"
+              :true-value="1"
+              :false-value="null"
+            />
+          </q-item-section>
+          <q-item-section>Bookmarks</q-item-section>
+        </q-item>
+
+        <q-item
+          v-ripple
+          tag="label"
+        >
+          <q-item-section side>
+            <q-checkbox
+              v-model="store.query.filter.favorites"
+              size="sm"
+              :true-value="1"
+              :false-value="null"
+            />
+          </q-item-section>
+          <q-item-section>Watchlist</q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </q-dialog>
 </template>
 
