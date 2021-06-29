5e15a6aabd6ff8182f6b2923eae18f3211412d8a
francoism90 <francoism90@users.noreply.github.com>
2021-06-29 12:18:42 +0200

    Only include fade animations

--- quasar.conf.js
+++ quasar.conf.js
@@ -111,7 +111,10 @@ module.exports = configure((ctx) => ({
 
   // animations: 'all', // --- includes all animations
   // https://v2.quasar.dev/options/animations
-  animations: [],
+  animations: [
+    'fadeIn',
+    'fadeOut',
+  ],
 
   // https://v2.quasar.dev/quasar-cli/developing-ssr/configuring-ssr
   ssr: {
