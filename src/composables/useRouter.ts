2543dbc6bce063b65d332e8ad4c909718ab3de8c
francoism90 <francoism90@users.noreply.github.com>
2021-09-21 15:47:01 +0200

    Remove currentRouteName

--- src/composables/useRouter.ts
+++ src/composables/useRouter.ts
@@ -3,11 +3,9 @@ import { computed } from 'vue';
 
 export default function useRouter() {
   const currentRoute = computed(() => router.currentRoute.value);
-  const currentRouteName = computed(() => currentRoute.value.name?.toString());
 
   return {
     router,
     currentRoute,
-    currentRouteName,
   };
 }
