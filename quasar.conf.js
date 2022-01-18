ce806cfcecadece9d63a36454462836580f8e4e4
francoism90 <francoism90@users.noreply.github.com>
2022-01-18 21:45:44 +0100

    wip

--- quasar.conf.js
+++ quasar.conf.js
@@ -86,6 +86,9 @@ module.exports = configure((ctx) => ({
 
   // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
   framework: {
+    config: {
+      dark: 'auto',
+    },
     iconSet: 'material-icons', // Quasar icon set
     lang: 'en-US', // Quasar language pack
 
