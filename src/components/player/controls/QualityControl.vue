2e6dfdc7e825d904b3c73f5a59b272cc3233b5ae
francoism90 <francoism90@users.noreply.github.com>
2022-01-19 20:31:13 +0100

    Add QualityControl

---
+++ src/components/player/controls/QualityControl.vue
@@ -0,0 +1,25 @@
+<template>
+  <template v-if="store.isLoading">
+    <q-spinner size="24px" />
+  </template>
+
+  <span class="text-weight-medium text-white">{{ resolution?.label }}</span>
+</template>
+
+<script lang="ts">
+import { usePlayer } from 'src/composables/usePlayer';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'QualityControl',
+
+  setup() {
+    const { store, resolution } = usePlayer();
+
+    return {
+      store,
+      resolution,
+    };
+  },
+});
+</script>
