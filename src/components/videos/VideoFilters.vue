470098c4aa9bd8a36f6df39d33196b462858897c
francoism90 <francoism90@users.noreply.github.com>
2022-02-14 15:54:21 +0100

    wip

--- src/components/videos/VideoFilters.vue
+++ src/components/videos/VideoFilters.vue
@@ -1,35 +1,50 @@
 <template>
-  <q-input
-    v-model.lazy="store.params.query"
-    :debounce="350"
-    autofocus
-    borderless
-    clearable
-    clear-icon="close"
-    dense
-    square
-    hide-bottom-space
-    placeholder="Search Videos"
-    class="input input-text tag-search full-width"
-  >
-    <template #prepend>
-      <q-icon name="search" />
-    </template>
-  </q-input>
+  <q-toolbar>
+    <q-select
+      v-model.lazy="store.params.sort"
+      :options="sorters"
+      behavior="menu"
+      dropdown-icon="expand_more"
+      menu-anchor="top left"
+      menu-self="top left"
+      borderless
+      emit-value
+      hide-bottom-space
+      map-options
+      options-dense
+      class="select"
+      popup-content-class="menu bordered"
+    >
+      <template #prepend>
+        Sort by
+      </template>
+    </q-select>
+
+    <q-space />
+  </q-toolbar>
 </template>
 
 <script lang="ts">
-import { useTags } from 'src/composables/useTags';
+import { useVideos } from 'src/composables/useVideos';
 import { defineComponent } from 'vue';
+
+const sorters = [
+  { label: 'Default', value: null },
+  { label: 'Most Recent', value: 'created:desc' },
+  { label: 'Most Viewed', value: 'views:desc' },
+  { label: 'Longest', value: 'duration:desc' },
+  { label: 'Shortest', value: 'duration:asc' },
+];
 
 export default defineComponent({
   name: 'VideoFilters',
 
   setup() {
-    const { store } = useTags();
+    const { store } = useVideos();
 
     return {
       store,
+      sorters,
     };
   },
 });
