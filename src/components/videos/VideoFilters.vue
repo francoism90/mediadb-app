ef16ae0283abdfc096417fe0115687ed19888ceb
francoism90 <francoism90@users.noreply.github.com>
2022-02-14 16:02:13 +0100

    wip

--- src/components/videos/VideoFilters.vue
+++ src/components/videos/VideoFilters.vue
@@ -1,32 +1,31 @@
 <template>
   <q-toolbar>
-    <q-select
-      v-model.lazy="store.params.sort"
-      :options="sorters"
-      behavior="menu"
-      dropdown-icon="expand_more"
-      menu-anchor="top left"
-      menu-self="top left"
-      borderless
-      emit-value
-      hide-bottom-space
-      map-options
-      options-dense
-      class="select"
-      popup-content-class="menu bordered"
-    >
-      <template #prepend>
-        Sort by
-      </template>
-    </q-select>
-
     <q-space />
+
+    <q-btn
+      class="btn-primary btn-bordered"
+      label="Filters"
+      icon="filter_list"
+      @click="visible = !visible"
+    />
   </q-toolbar>
+
+  <q-slide-transition>
+    <div v-show="visible">
+      <nav class="q-pt-lg">
+        <div class="filter-form row q-col-gutter-md">
+          <filter-list class="col-xs-12 col-sm-12 col-md-4" />
+          <filter-duration class="col-xs-12 col-sm-12 col-md-4" />
+          <filter-sorter class="col-xs-12 col-sm-12 col-md-4" />
+        </div>
+      </nav>
+    </div>
+  </q-slide-transition>
 </template>
 
 <script lang="ts">
 import { useVideos } from 'src/composables/useVideos';
-import { defineComponent } from 'vue';
+import { defineAsyncComponent, defineComponent, ref } from 'vue';
 
 const sorters = [
   { label: 'Default', value: null },
@@ -39,12 +38,21 @@ const sorters = [
 export default defineComponent({
   name: 'VideoFilters',
 
+  components: {
+    FilterDuration: defineAsyncComponent(() => import('components/videos/FilterDuration.vue')),
+    FilterList: defineAsyncComponent(() => import('components/videos/FilterList.vue')),
+    FilterSorter: defineAsyncComponent(() => import('components/videos/FilterSorter.vue')),
+  },
+
   setup() {
     const { store } = useVideos();
+
+    const visible = ref<boolean>(false);
 
     return {
       store,
       sorters,
+      visible,
     };
   },
 });
