c8c212dab09d4e840b6868d2e8b2274c7c12afa1
francoism90 <francoism90@users.noreply.github.com>
2022-03-01 17:40:16 +0100

    wip

--- src/composables/useSession.ts
+++ src/composables/useSession.ts
@@ -1,5 +1,6 @@
 import { includes } from 'lodash';
 import { echoKey } from 'src/boot/echo';
+import { useModels } from 'src/composables/useModels';
 import { SessionState } from 'src/interfaces';
 import { authenticate, check, destroy } from 'src/services/auth';
 import { computed, inject, reactive, readonly } from 'vue';
@@ -8,6 +9,7 @@ const state = reactive(<SessionState>{});
 
 export const useSession = () => {
   const echo = inject(echoKey);
+  const { deleted, replaced } = useModels();
 
   const roles = computed(() => state.user?.roles || []);
   const permissions = computed(() => state.user?.permissions || []);
@@ -15,17 +17,18 @@ export const useSession = () => {
   const hasRole = (key: string | string[]) => includes(roles.value, key);
   const hasPermission = (key: string | string[]) => includes(permissions.value, key);
 
-  // const unsubscribe = () => echo?.leave(`user.${state.user?.id}`);
-  // const subscribe = () => echo?.private(`user.${state.user?.id}`)
-  //   ?.listen('.model.favorited', updated)
-  //   ?.listen('.model.followed', updated);
+  const unsubscribe = () => echo?.leave(`user.${state.user?.id}`);
+
+  const subscribe = () => echo?.private(`user.${state.user?.id}`)
+    ?.listen('.model.favorited', deleted)
+    ?.listen('.model.followed', replaced);
 
   return {
     authenticate,
     check,
     destroy,
-    // subscribe,
-    // unsubscribe,
+    subscribe,
+    unsubscribe,
     hasRole,
     hasPermission,
     state: readonly(state),
