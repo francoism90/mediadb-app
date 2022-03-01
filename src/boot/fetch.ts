adb621f9ecdd01ba4764581c5a7b08a7c8d868df
francoism90 <francoism90@users.noreply.github.com>
2022-03-01 12:50:35 +0100

    wip

--- src/boot/fetch.ts
+++ src/boot/fetch.ts
@@ -1,8 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import { api } from 'src/services/api';
 
-export const fetch = api;
-
 export default boot(({ app }) => {
   app.config.globalProperties.$api = api;
   app.config.globalProperties.$fetch = api;
