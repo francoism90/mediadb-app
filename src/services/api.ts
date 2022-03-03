07c51662757b4f169f9782a7e740e22a4bb89b95
francoism90 <francoism90@users.noreply.github.com>
2022-03-03 17:35:37 +0100

    wip

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,11 +2,13 @@ import { createFetch } from '@vueuse/core';
 import { set } from 'lodash';
 import { getToken } from 'src/services/auth';
 
+export const token = getToken() || '';
+
 export const api = createFetch({
   baseUrl: process.env.API_URL,
   options: {
     beforeFetch({ options }) {
-      set(options, 'headers.Authorization', `Bearer ${getToken() || ''}`);
+      set(options, 'headers.Authorization', `Bearer ${token}`);
       set(options, 'headers.Content-Type', 'application/json');
       set(options, 'headers.X-Requested-With', 'XMLHttpRequest');
 
@@ -18,9 +20,10 @@ export const api = createFetch({
 export const uri = createFetch({
   options: {
     beforeFetch({ options }) {
-      set(options, 'headers.Authorization', `Bearer ${getToken() || ''}`);
+      set(options, 'headers.Authorization', `Bearer ${token}`);
       set(options, 'headers.Content-Type', 'application/json');
       set(options, 'headers.X-Requested-With', 'XMLHttpRequest');
+
       return { options };
     },
   },
